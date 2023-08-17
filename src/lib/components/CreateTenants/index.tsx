e39ef0facd11e0fa4a46525652bd12b71b2d2bbc
fatima <marquezlinares19@gmail.com>
2023-08-16 22:27:22 -0500

    component en proceso

--- src/lib/components/CreateTenants/index.tsx
+++ src/lib/components/CreateTenants/index.tsx
@@ -1,3 +1,9 @@
+import { ApplicationFee } from '../ApplicationFee';
+import { Button } from '../Button';
+import { CreatePersonalDetails } from '../CreatePersonalDetails';
+import { CreateScreeningChecks } from '../CreateScreeningChecks';
+import { MessageDetails } from '../MessageDetails';
+import { SendVia } from '../SendVia';
 import { Text } from '../Text';
 
 export interface CreateTenantsProps {
@@ -14,6 +20,30 @@ export const CreateTenants = ({
                 within your organization or business. By creating tenants, you
                 can organize your resources, and manage them efficiently.
             </Text>
+            <div className='CreatePersonalDetails'>
+                <CreatePersonalDetails />
+            </div>
+            <div className=' CreateScreeningChecks'>
+                <CreateScreeningChecks
+                />
+            </div>
+
+            <div className='MessageDetails'>
+                <MessageDetails />
+            </div>
+            <div className=' ApplicationFee'>
+                <ApplicationFee />
+
+            </div>
+            <div className='SendVia'>
+                <SendVia />
+            </div>
+            <Button styleTemplate='btn2'>
+                Send
+            </Button>
+
+
+
         </div>
     );
 };
